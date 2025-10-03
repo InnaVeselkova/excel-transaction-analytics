0e2570142df51706c858cb8b35926fe5a28ce473
Inna <MenelN@yandex.ru>
2025-10-03 23:50:42 +0300

    add logging for services

--- src/services.py
+++ src/services.py
@@ -2,7 +2,7 @@ import json
 import re
 from typing import Dict, List
 
-from src.utils import read_excel_file
+from src.utils import read_excel_file, logger
 
 excel_path = "../data/operations.xlsx"
 
@@ -12,14 +12,22 @@ def filter_personal_transfers(data_list: List[Dict]) -> str:
     Функция фильтрует транзакции, относящиеся к переводам физлицам
     """
 
+    logger.info("Начало фильтрации транзакций по категориям и описанию.")
     pattern = re.compile(r'\b[А-ЯЁ][а-яё]+ [А-ЯЁ]\.')
 
-    filtered = [
-        transaction for transaction in data_list
-        if transaction.get('Категория') == 'Переводы' and pattern.search(transaction.get('Описание', ''))
+    try:
+        filtered = [
+            transaction for transaction in data_list
+            if transaction.get('Категория') == 'Переводы' and pattern.search(transaction.get('Описание', ''))
         ]
+        logger.info(f"Отфильтровано транзакций: {len(filtered)} из {len(data_list)}.")
+    except Exception as e:
+        logger.error(f"Ошибка при фильтрации транзакций: {e}")
+        raise
 
-    return json.dumps(filtered, ensure_ascii=False, indent=2)
+    result_json = json.dumps(filtered, ensure_ascii=False, indent=2)
+    logger.info("Фильтрация завершена.")
+    return result_json
 
 
 if __name__ == "__main__":
