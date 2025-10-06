8a13134b75368006d024dc15623090c50851258c
Inna <MenelN@yandex.ru>
2025-10-06 17:30:59 +0300

    add test for utils

--- test/conftest.py
+++ test/conftest.py
@@ -1,9 +1,14 @@
 import pytest
+import pandas as pd
 
 
 @pytest.fixture
-def
+def sample_excel():
+    file_path = "../data/sample.xlsx"
+    df = pd.DataFrame({
+        'Column1': [1, 2],
+        'Column2': ['A', 'B']
+    })
+    df.to_excel(file_path, index=False)
 
-
-@pytest.fixture
-def
+    return file_path
